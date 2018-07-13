65813b80a712afde3a09411fdfcd14e7adf30205
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-13 17:09:08 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,6 +46,15 @@ class Dog
     result.size > 0 ? self.new_from_db(result.first) : nil
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM dogs
+    WHERE name = ?
+    SQL
+    result = DB[:conn].execute(sql, name, breed)
+    result.size > 0 ? self.new_from_db(result.first) : nil
+  end
+
   def self.new_from_db(row)
     dog = self.new(id:row[0], name:row[1], breed:row[2])
   end
