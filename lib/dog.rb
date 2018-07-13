41fa3fdba1f0f185d48b9770205a8360b80ea7bb
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-13 17:13:44 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -21,7 +21,7 @@ class Dog
   def update
     sql = <<-SQL
     UPDATE dogs
-    SET name = ?, breed = ? 
+    SET name = ?, breed = ?
     WHERE id = ?
     SQL
     DB[:conn].execute(sql)
@@ -67,6 +67,9 @@ class Dog
 
   def self.new_from_db(row)
     dog = self.new(id:row[0], name:row[1], breed:row[2])
+    if row[1] = "Teddy Jr."
+      binding.pry
+    end
   end
 
   def self.create_table
