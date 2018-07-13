690959202405959e2102daf68f9281d3f90e6adf
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-13 16:23:16 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,40 @@
+class Dog
+  attr_accessor :name, :breed, :id
+
+  def initialize(attributes, id = nil)
+    attributes.each {|key, value| self.send(("#{key}="),value) }
+  end
+
+  def save
+    if id != nil
+       self.update
+    else
+      sql = <<-SQL
+       INSERT INTO dogs (name, breed) VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql,[@name, @breed])
+      @id = DB[:conn].execute("SELECT last_insert_rowid()")[0][0]
+      self
+    end
+  end
+
+  def self.create(name:,breed:)
+    binding.pry
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE dogs"
+    DB[:conn].execute(sql)
+  end
+end
