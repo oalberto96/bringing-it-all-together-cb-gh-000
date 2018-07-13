3628d2bce0ff32f2feda23291e54f47c1674fb81
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-13 16:53:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,7 +25,7 @@ class Dog
 
   def self.find_by_id(id)
     sql = <<-SQL
-    SELECT * FROM dogs 
+    SELECT * FROM dogs
     WHERE dogs.id = ?
     SQL
     row = DB[:conn].execute(sql,id)
@@ -33,7 +33,7 @@ class Dog
   end
 
   def self.create_from_database(row)
-    
+    self.new(id:row[0], name:row[1], name:row[2])
   end
 
   def self.create_table
